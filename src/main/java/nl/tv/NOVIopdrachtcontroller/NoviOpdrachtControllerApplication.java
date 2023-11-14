a1f14a37b06916781979df3026cc9b9a2166ff79
tamer <talashraf@snelstart.nl>
2023-11-14 10:52:35 +0100

    Initial commit

---
+++ src/main/java/nl/tv/NOVIopdrachtcontroller/NoviOpdrachtControllerApplication.java
@@ -0,0 +1,13 @@
+package nl.tv.NOVIopdrachtcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoviOpdrachtControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoviOpdrachtControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/tv/NOVIopdrachtcontroller/NoviOpdrachtControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.tv.NOVIopdrachtcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoviOpdrachtControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
